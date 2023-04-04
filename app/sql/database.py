74530e02c01a2ef44b4762b121f1b32303b852a8
rafaski1 <rav.skibinski@gmail.com>
2023-04-04 18:11:03 +0200

    first commit

--- app/sql/database.py
+++ app/sql/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.settings import DATABASE_URL
 
-# SQLite
+# Switch to SQLite if unable connecting to Postgres
 # DATABASE_URL = "sqlite:///./sql_app.db"
 # engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
