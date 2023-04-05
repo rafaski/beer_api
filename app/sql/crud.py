1d50ba2adceeeac85a44998433d3e602dbef3579
rafaski1 <rav.skibinski@gmail.com>
2023-04-05 08:51:23 +0200

    first commit

--- app/sql/crud.py
+++ app/sql/crud.py
@@ -22,6 +22,10 @@ def create_hop(db: Session, hop: Hop) -> None:
 def get_avg_temp_by_hops(db: Session) -> list[dict]:
     """
     Get an average fermentation temperature by hop
+
+    Logic:
+    Joins the beers and hops tables, groups by the hop name,
+    and calculates the average fermentation temperature.
     """
     results = db.query(models.Hop.name, func.round(func.avg(
         models.Beer.fermentation_temp), 1).label(
@@ -35,6 +39,16 @@ def get_avg_temp_by_hops(db: Session) -> list[dict]:
 def get_avg_temp_primary_hops(db: Session) -> list[dict]:
     """
     Get average (mean) fermentation temperature for the primary hops
+
+    Logic:
+    primary_query: get primary hop for each beer along with its maximum amount.
+    Some beers had the same hops within a single recipe,
+    I made sure to calculate the sum first,
+    then showcase only those with the highest value per beer.
+    Some beers have several hops with the same amount as showcased
+    in the example data in README.md.
+    secondary_query: calculates average fermentation temperature for each hop.
+    results: joined the above subqueries to showcase results.
     """
     # Calculate the primary hop for each beer along with its maximum amount
     primary_query = db.query(
@@ -72,6 +86,11 @@ def get_avg_temp_primary_hops(db: Session) -> list[dict]:
 def get_ten_most_used_hops(db: Session) -> dict:
     """
     Show the top 10 most used hops in the recipes
+
+    Logic:
+    Get hop.name, sum the hop.amount column and round it.
+    Group results by hop.name. Sort results in descending order
+    based on the rounded sum of the amount column and limit results to top 10.
     """
     results = db.query(models.Hop.name, func.round(func.sum(
         models.Hop.amount), 1).label('total_amount')).group_by(
@@ -83,6 +102,10 @@ def get_ten_most_used_hops(db: Session) -> dict:
 def get_beers_by_temp(db: Session, temp: int) -> list[dict]:
     """
     Get all beers that have a fermentation temperature greater than X
+
+    Logic:
+    Query beers table. Filter results by privided temp: beer.temp > temp.
+    Sort results by name.
     """
     results = db.query(models.Beer).filter(
         models.Beer.fermentation_temp > temp).order_by(
@@ -93,6 +116,10 @@ def get_beers_by_temp(db: Session, temp: int) -> list[dict]:
 def get_hops_by_amount(db: Session, amount: int) -> list[dict]:
     """
     Get all hops that have an amount greater than or equal to X
+
+    Logic:
+    Query hops table. Filter results by provided amount: hop.amount >= amount.
+    Sort in descending order by the amount column.
     """
     results = db.query(models.Hop).filter(
         models.Hop.amount >= amount).order_by(
@@ -104,6 +131,10 @@ def get_beers_by_hop(db: Session, hop_name: str) -> list[dict]:
     """
     Get all beers that have a hop with the name X
     and order them by fermentation temperature
+
+    Logic:
+    Join beers and hops tables. Filter hop.name by provided hop_name.
+    Order results by beer.fermentation_temp
     """
     results = db.query(models.Beer).join(models.Hop).filter(
         models.Hop.name == hop_name).order_by(
